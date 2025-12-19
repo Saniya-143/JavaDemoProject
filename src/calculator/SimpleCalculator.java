009c642557c72b15c1ae2a4950178d9801085e17
saniya shaik <itmesaniya7@gmail.com>
2025-12-19 19:04:09 +0530

    Function defintion

--- src/calculator/SimpleCalculator.java
+++ src/calculator/SimpleCalculator.java
@@ -2,18 +2,18 @@ package calculator;
 
 public class SimpleCalculator extends Calculator {
     public Integer sum(int a, int b) {
-        return 0;
+        return a+b;
     }
 
     public Integer sub(int a, int b) {
-        return 0;
+        return a-b;
     }
 
     public Integer multi(int a, int b) {
-        return 0;
+        return a*b;
     }
 
     public Integer div(int a, int b) {
-        return 0;
+        return a/b;
     }
 }
